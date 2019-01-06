d4d0e876472d873fffc3249592d4e22760c7fc6f
James Balamuta <balamut2@illinois.edu>
2019-01-05 18:50:54 -0600

    Need the OpenMP header file...

--- src/my_c_parallel_func.c
+++ src/my_c_parallel_func.c
@@ -1,6 +1,10 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 void parallel_function(double *x, int n) {   
   int i;
   #pragma omp parallel for firstprivate(x, n)
