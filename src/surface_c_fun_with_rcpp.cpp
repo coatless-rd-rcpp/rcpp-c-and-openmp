61f419e41bd255f82bdab3cc44d6e848fe55c439
james.balamuta@gmail.com <james.balamuta@gmail.com>
2026-06-25 13:56:50 -0500

    Enable OpenMP for the C routine and correct multiply_by_two docs
    The #pragma omp lives in a C translation unit, but Makevars only set
    PKG_CXXFLAGS, so the C compiler never received the OpenMP flag and the loop
    compiled serially. Add PKG_CFLAGS = $(SHLIB_OPENMP_CFLAGS) and mirror it in
    Makevars.win. Also correct the roxygen for multiply_by_two: n is the number of
    elements of x to process, not a core count. Regenerate R/RcppExports.R and the
    man page.

--- src/surface_c_fun_with_rcpp.cpp
+++ src/surface_c_fun_with_rcpp.cpp
@@ -5,24 +5,24 @@ extern "C" {
   void parallel_function(double *x, int n);
 }
 
-//' Multiple a Value By 2
-//' 
-//' Computes the multiplication of 2 by passing the data into a _C_ routine
-//' that is parallelized
-//' 
-//' @param x A `vector` of numeric values to multiple.
-//' @param n The number of cores to use.
-//' 
+//' Multiply a Vector by Two
+//'
+//' Doubles each element of a numeric vector by passing the data into a
+//' parallelized _C_ routine.
+//'
+//' @param x A `numeric` vector of values to multiply.
+//' @param n The number of elements of `x` to process.
+//'
 //' @examples
-//' 
+//'
 //' # Data
 //' x = 1:5
-//' 
-//' # Number of Cores to use in Parallelization
-//' ncores = 2
-//' 
+//'
+//' # Process all elements of x
+//' n = length(x)
+//'
 //' # Call the routine
-//' multiply_by_two(x, n = ncores)
+//' multiply_by_two(x, n = n)
 //' @export
 // [[Rcpp::export]]
 void multiply_by_two(Rcpp::NumericVector x, int n) {
